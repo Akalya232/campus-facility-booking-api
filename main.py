a09ce5258f1c0ae1f6a7575b03e17cea7742e549
Akalya R <itzakalya@gmail.com>
2026-09-04 21:23:40 +0530

    Day 4: Implement user creation workflow

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+
+from database import SessionLocal
+from models import User
+from schemas import UserCreate, UserResponse
 
 app = FastAPI()
+
+# Database session
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.post("/users", response_model=UserResponse, status_code=201)
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
+
+    new_user = User(
+        name=user.name,
+        email=user.email
+    )
+
+    db.add(new_user)
+
+    try:
+        await db.commit()
+        await db.refresh(new_user)
+
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=409,
+            detail="Email already exists"
+        )
+
+    return new_user
